e81fb2c00f3fdb2aeca47244a70df4fc994ea7e4
FaizaIftikhar <iamfaiza8o9@gmail.com>
2025-09-24 15:21:32 +0100

    feat: protect /users and /about routes with ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
-// App.jsx
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import LoginForm from "./pages/LoginForm";
 import UserList from "./pages/UserList";
-import UserDetails from "./pages/UserDetails";
-import NotFound from "./pages/NotFound";
-
+import UserDetails from "./pages/UserDetails"; // if you made this
+import NotFound from "./pages/NotFound"; // if you made this
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/user" element={<UserList />} />
-        <Route path="/users/:id" element={<UserDetails />} />
-        <Route path="/about" element={<About />} />
+        <Route
+          path="/login"
+          element={<LoginForm setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <UserList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users/:id"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <UserDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <About />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
